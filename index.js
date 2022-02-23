3183d6b830330f6d50dbae8bd937274c44ff2b03
Kim Green <kimgreen.writings@gmail.com>
2022-02-24 01:42:39 +0200

    commit tree 70c147af362a7a04050c70851ddc45b20c3c4596

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun  (skate = 'roller-skate') {
+    return (`This Saturday, I want to ${skate}!`);
+}
+saturdayFun();
+saturdayFun('bathe my dog');
+
+function mondayWork(office = 'go to the office') {
+    return (`This Monday, I will ${office}.`)
+}
+mondayWork();
+mondayWork('work from home');
+
+function wrapAdjective (flair ="*"){
+    return function(adj = 'special') {
+        return (`You are ${flair}${adj}${flair}!`)
+    }
+}
